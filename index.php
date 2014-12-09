19ab88988831eef0c6cfb995c1708f6e40ef43ed
Ziga Vukcevic <ziga@be-codified.com>
2014-12-10 00:12:13 +0100

    initial html structure commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>be-codified.com | freelance front-end developer</title>
+    </head>
+    <body>
+        <h1>be-codified.com</h1>
+</body>
+</html>
