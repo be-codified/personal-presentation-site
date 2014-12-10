de7119e69a1528458ed0a6668e0a3520f5ddace4
Ziga Vukcevic <ziga@be-codified.com>
2014-12-10 23:42:52 +0100

    added bootstrap minified css file

--- index.php
+++ index.php
@@ -5,6 +5,10 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>be-codified.com | freelance front-end developer</title>
+
+        <!-- stylesheets -->
+
+        <link href="css/bootstrap.min.css" rel="stylesheet">
     </head>
     <body>
         <h1>be-codified.com</h1>
