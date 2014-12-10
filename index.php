247d6ad910ffa748eb1dcb0566f91c93d542cbaf
Ziga Vukcevic <ziga@be-codified.com>
2014-12-11 00:47:06 +0100

    added links to icons, markup cleaning

--- index.php
+++ index.php
@@ -1,20 +1,27 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="description" content="" /> 
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>be-codified.com | freelance front-end developer</title>
 
         <!-- stylesheets -->
-        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link href="css/bootstrap.min.css" rel="stylesheet" media="screen" />
+
+        <!-- icons -->
+        <link rel="shortcut icon" type="image/ico" href="images/favicon.ico" />
+        <link rel="apple-touch-icon" href="images/apple-touch-icon.png" />
+        <link rel="apple-touch-icon" sizes="72x72" href="images/apple-touch-icon-72.png" />
+        <link rel="apple-touch-icon" sizes="114x114" href="images/apple-touch-icon-114.png" />
 
         <!-- IE8 support for html5 elements and media queries -->
-        <script src="js/lib/html5shiv.min.js"></script>
-        <script src="js/lib/respond.min.js"></script>
+        <script type="text/javascript" src="js/lib/html5shiv.min.js"></script>
+        <script type="text/javascript" src="js/lib/respond.min.js"></script>
 
         <!-- google analytics tracking code -->
-        <script>
+        <script type="text/javascript">
             (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
             (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
             m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -28,6 +35,6 @@
         <h1>be-codified.com</h1>
 
         <!-- jquery -->
-        <script src="js/lib/jquery.min.js"></script>
+        <script type="text/javascript" src="js/lib/jquery.min.js"></script>
     </body>
 </html>
