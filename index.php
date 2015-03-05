9cb68ec7e964bd20b0436c19c1164831c9a43066
Ziga Vukcevic <ziga@be-codified.com>
2015-03-05 11:36:13 +0100

    added portfolio items, changed column widths for large screens

--- index.php
+++ index.php
@@ -33,14 +33,14 @@
         <div class="container-fluid">
             <article>
                 <div class="row main-color-1">
-                    <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 col-lg-push-2">
-                        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-8 intro">
+                    <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+                        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-10 col-lg-offset-2 intro">
                             <header>
                                 <h1>
                                     <a href="http://www.be-codified.com" title="be-codified.com">be-codified.com</a>
                                 </h1>
                             </header>
-                            <h2>Hi, my name is Žiga and I'm a <strong>front-end developer</strong>.</h2>
+                            <h2>Hi,<br />my name is Žiga and I'm a <strong>front-end developer</strong>.</h2>
                             <?php require_once('time_period.php'); ?>
                             <p class="lead">I have been working in the web industry for the past <?php echo time_period($date_webindustry, $date_today, 0); ?> years, for the last <?php echo time_period($date_freelancing, $date_today, 0); ?> years as a freelancer.</p>
                         </div>
@@ -75,7 +75,7 @@
                 </div>
                 <div class="row">
                     <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 main-color-2">
-                        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-10 col-lg-offset-2">
+                        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-11 col-lg-offset-1">
                             <h2>My main strenghts</h2>
                             <ul class="plus-bullets strenghts">
                                 <li>strong understanding of developing for the web and <strong class="reduced-right-padding">a careful eye for design</strong>,</li>
@@ -94,8 +94,8 @@
                     </div>
                 </div>
                 <div class="row main-color-4">
-                    <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 col-lg-push-2 portfolio item-1">
-                        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-8">
+                    <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 col-lg-push-1 portfolio item-1">
+                        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-10">
                             <p class="latest">Latest work</p>
                             <h2>Consumer federation of Slovenia</h2>
                             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque et efficitur ipsum. Fusce accumsan congue diam, et bibendum mauris aliquam</p>
@@ -103,18 +103,16 @@
                         </div>
                     </div>
                     <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 main-color-1">
-                        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-8">
+                        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
                         <h2>My main skills</h2>
                             <ul class="plus-bullets skills">
-                                <li><strong>HTML5</strong> (modern markup),</li>
-                                <li><strong>CSS3</strong> (modern properties, web font embeding, SVG),</li>
-                                <li><strong>cross-browser</strong> (i.e. Chrome, Firefox, Safari, Internet Explorer 9+) and <strong>cross-platform</strong> (Android, iOS iPhone/iPad) <strong class="reduced-right-padding">compatibility testing</strong>,</li>
-                                <li><strong>jQuery</strong> (DOM elements manipulation, handling events, JSON parsing and using Ajax) and <strong>JavaScript</strong> (// note: put something here),</li>
-                                <li><strong>Git</strong> (working with a team of developers, service BitBucket),</li>
-                                <li>experienced in <strong>frameworks or theme starters</strong> (i.e. Bootstrap, Html5Boilerplate, _s),</li>
+                                <li><strong>HTML5</strong> (modern markup), <strong>CSS3</strong> (modern properties, web font embeding, SVG, animations) with CSS pre-processor <strong>LESS</strong></li>
                                 <li><strong>graphic tools</strong> (i.e. Adobe Photoshop and Illustrator),</li>
+                                <li>experienced in <strong>frameworks or theme starters</strong> (i.e. Bootstrap, Html5Boilerplate, _s),</li>
+                                <li><strong>jQuery</strong> (DOM elements manipulation, handling events, JSON parsing and using Ajax) and <strong>JavaScript</strong></li>
+                                <li><strong>cross-browser</strong> (i.e. Chrome, Firefox, Safari, Internet Explorer 9+) and <strong>cross-platform</strong> (Android, iOS iPhone/iPad) <strong class="reduced-right-padding">compatibility testing</strong>,</li>
+                                <li><strong>Git</strong> (working with a team of developers, service GitHub and BitBucket),</li>
                                 <li>basic understanding of PHP and MySQL</li>
-                                <li>// note: AngularJS, Less, GitHub, Rest API, animations?</li>
                             </ul>
                         </div>
                     </div>
@@ -129,7 +127,7 @@
                         </div>
                     </div>
                     <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 main-color-1">
-                        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-8 col-lg-offset-4">
+                        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-10 col-lg-offset-2">
                             <h2>Magical tools I use on a daily basis</h2>
                             <ul class="plus-bullets tools">
                                 <li>sophisticated text editor <a href="http://www.sublimetext.com/" target="_blank">Sublime Text 2</a><br /><span class="warning"><span>Warning:</span> You can easly fall in love with. Proceed to link with caution!</span></li>
@@ -143,16 +141,15 @@
                                     <a href="http://jquery.com/" target="_blank">jQuery</a> &ndash; fast, small, and feature-rich JavaScript library
                                 </li>
                                 <li>debugging tools <a href="https://developer.chrome.com/devtools" target="_blank">Chrome DevTools</a> built into Google Chrome</li>
-                                <li>JavaScript task runner <a href="http://gruntjs.com/" target="_blank">Grunt</a> (note: what libraries should be here?)</li>
-                                <li>all running on a small thing called Apple MacBook Pro 13</i>, extended to an external 24-inch monitor</li>
-                                <li>//note: animations?</li>
+                                <li>JavaScript task runner <a href="http://gruntjs.com/" target="_blank">Grunt</a> (e.g. watch, livereload, compiling from LESS, CSS minify, HTML and CSS validation, deployment)</li>
+                                <li>all running on a small thing called <a href="http://www.apple.com/macbook-pro/" target="_blank">Apple MacBook Pro 13</a></i>, extended to an external 24-inch monitor</li>
                             </ul>
                         </div>
                     </div>
                 </div>
                 <div class="row main-color-4">
                     <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 portfolio item-3">
-                        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-8 col-lg-offset-4">
+                        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-10 col-lg-offset-2">
                             <p class="latest">Latest work</p>
                             <h2>iPhone giveaway</h2>
                             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque et efficitur ipsum. Fusce accumsan congue diam, et bibendum mauris aliquam</p>
@@ -160,7 +157,7 @@
                         </div>
                     </div>
                     <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 main-color-1">
-                        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-8">
+                        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
                             <h2>Want to hire me for a project?</h2>
                             <p>Please feel free to contact me by any communication channel:</p>
                             <ul class="contacts">
@@ -182,7 +179,7 @@
                 </div>
                 <div class="row">
                     <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 main-color-2">
-                        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-10 col-lg-offset-2 how-made">
+                        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-11 col-lg-offset-1 how-made">
                             <h2 class="smaller">How was this site made?</h2>
                             <p class="first">No website template has been used. I have coded it with modern technologies:</p>
                             <ul class="plus-bullets">
@@ -196,7 +193,7 @@
                                 <li>time for how long I am working in web industry is calculated automatically.// note: change this</li>
                             </ul>
                             <p>
-                                Design made with <i class="fa fa-heart fa-lg"></i><span class="reference-point"><span class="bubble">The icon reads as love.</span></span> by Groga Tancek and excellent profile <i class="fa fa-camera fa-lg"></i><span class="reference-point"><span class="bubble">The icon reads as photo.</span></span> provided by Ana Skobe.
+                                Design made with <i class="fa fa-heart fa-lg"></i><span class="reference-point"><span class="bubble">The icon should read as love.</span></span> by Groga Tancek and excellent profile <i class="fa fa-camera fa-lg"></i><span class="reference-point"><span class="bubble">The icon should read as photo.</span></span> provided by Ana Skobe.
                             </p>
                             <p class="last">Feel free to see whole project code at <i class="fa fa-github-square fa-2x"></i> <a href="https://github.com/be-codified/personal-presentation-site" target="_blank">GitHub</a> site.</p>
                         </div>
