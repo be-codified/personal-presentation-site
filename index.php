6279f8a9f80e50e4337757da25593072f741d4a3
Ziga Vukcevic <ziga@be-codified.com>
2015-03-08 18:36:29 +0100

    playing with "image bottom alignment" issue

--- index.php
+++ index.php
@@ -93,18 +93,19 @@
                         </div>
                     </div>
                 </div>
-                <div class="row main-color-4 equal-columns-height">
-                    <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 col-lg-push-1 equal-col portfolio item-1">
-                        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-10">
+                <div class="row main-color-4">
+                    <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 col-lg-push-1 portfolio item-1" style="background-color: red;">
+                        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-10" style="background-color: blue;">
                             <p class="latest">Latest work</p>
                             <h2>Consumer federation of Slovenia</h2>
                             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque et efficitur ipsum. Fusce accumsan congue diam, et bibendum mauris aliquam</p>
-                            <a href="#" class="more pull-right"></a>
+                            <img class="col-lg-12" src="img/portfolio-1.png" />
+                            <a href="#" class="more"></a>
                         </div>
                     </div>
-                    <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 main-color-1 equal-col">
+                    <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 main-color-1">
                         <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-                        <h2>My main skills</h2>
+                            <h2>My main skills</h2>
                             <ul class="plus-bullets skills">
                                 <li><strong>HTML5</strong> (modern markup), <strong>CSS3</strong> (modern properties, web font embeding, SVG, animations) with CSS pre-processor <strong>LESS</strong></li>
                                 <li><strong>graphic tools</strong> (i.e. Adobe Photoshop and Illustrator),</li>
