09e0fe27983d75379b1c03cffe92ec2e324d2d5f
Ziga Vukcevic <ziga@be-codified.com>
2015-04-07 19:15:19 +0200

    minified plugin

--- index.php
+++ index.php
@@ -232,7 +232,7 @@
     
         <!-- jquery, animations and main script -->
         <script type="text/javascript" src="js/lib/jquery.min.js"></script>
-        <script type="text/javascript" src="js/lib/jquery.easing.1.3.js"></script>
+        <script type="text/javascript" src="js/lib/jquery.easing.1.3.min.js"></script>
         <script type="text/javascript" src="js/lib/jquery.animateNumber.min.js"></script>
         <script type="text/javascript" src="js/main.min.js"></script>
     </body>
